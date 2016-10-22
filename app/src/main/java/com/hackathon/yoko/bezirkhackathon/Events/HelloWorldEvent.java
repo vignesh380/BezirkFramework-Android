2de5bf5c36ab1734f9e6eb458973dd520f4b2fb5
Vignesh Shankar <vicky380@gmail.com>
2016-10-22 14:48:36 -0400

    sample code for bezirk

---
+++ app/src/main/java/com/hackathon/yoko/bezirkhackathon/Events/HelloWorldEvent.java
@@ -0,0 +1,11 @@
+package com.hackathon.yoko.bezirkhackathon.Events;
+
+import com.bezirk.middleware.messages.Event;
+
+/**
+ * Created by vicky on 10/22/2016.
+ */
+
+public class HelloWorldEvent extends Event{
+    public String helloMessage;
+}
--- app/src/main/java/com/hackathon/yoko/bezirkhackathon/MainActivity.java
+++ app/src/main/java/com/hackathon/yoko/bezirkhackathon/MainActivity.java
@@ -2,12 +2,43 @@ package com.hackathon.yoko.bezirkhackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.android.BezirkMiddleware;
+import com.hackathon.yoko.bezirkhackathon.Events.HelloWorldEvent;
 
 public class MainActivity extends AppCompatActivity {
+    private Bezirk bezirk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instantiate event with custom data
+        HelloWorldEvent helloWorldEvent = new HelloWorldEvent();
+        helloWorldEvent.helloMessage = "Hi world, how are you?";
+
+        //Initialize the Bezirk service
+        BezirkMiddleware.initialize(this);
+
+        //Register with BezirkMiddleware to get an instance of Bezirk API.
+        //The parameter is any human-readable string for a name of your Zirk
+        bezirk = BezirkMiddleware.registerZirk("Hello World Zirk");
+
+        //Give Bezirk 2 seconds to fully initialize before sending an event
+        try { Thread.sleep(2000); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+
+        //Send the Hello World event
+        bezirk.sendEvent(helloWorldEvent);
+
+        //Display a confirmation message
+        TextView t = (TextView) findViewById(R.id.activity_main_hello);
+        t.setText("Published hello world message: " + helloWorldEvent.helloMessage);
+
+        //t.setText("Yoco Broah!!");
     }
 }
